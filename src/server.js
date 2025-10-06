9bbfc348e527e4e6d9b0c4a744d0c37fbb8d6a5f
Nash664 <nashirferoz01@gmail.com>
2025-10-06 16:32:19 -0700

    Updated some features

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const start = async () => {
   await connectDB(process.env.MONGO_URI);
   app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
   });
 };
 
